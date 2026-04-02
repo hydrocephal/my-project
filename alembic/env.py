f5111199d1e6a8b4aa6ce178b68d3bd839a2604d
hydrocephal <braindamaged10236@gmail.com>
2026-04-02 17:06:20 +0300

    ruff+pre-commit hook

--- alembic/env.py
+++ alembic/env.py
@@ -6,10 +6,9 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-
-from app.db.database import Base
-from app.models.models import User, Message
 from app.core.config import settings
+from app.db.database import Base
+from app.models.models import Message, User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
