cfa8bddb3236886781c45c0732ff09e1cd5f12b8
hydrocephal <braindamaged10236@gmail.com>
2026-04-08 18:10:10 +0300

    fix entrypoint for VPS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-alembic upgrade head
-exec gunicorn app.main:app -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+/app/.venv/bin/alembic upgrade head
+exec /app/.venv/bin/gunicorn app.main:app -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
